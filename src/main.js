7e40bed113bcb0a1d685ed8e44f07582effef6da
Alex Cui <cuizhihui030925@outlook.com>
2021-01-10 09:46:18 +0800

    Finished 404 page.

--- src/main.js
+++ src/main.js
@@ -15,8 +15,8 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 
 import App from './App.vue';
 import Index from './views/Index.vue';
-import ContestList from './views/ContestList.vue';
 import Login from './views/Login.vue';
+import Page404 from './views/404.vue';
 import Problem from './views/Problem.vue';
 import ProblemEditor from './views/ProblemEditor.vue';
 import ProblemList from './views/ProblemList.vue';
@@ -28,14 +28,14 @@ import config from './config';
 
 const routes = [
     { path: '/', component: Index },
-    { path: '/contest', component: ContestList },
     { path: '/login', component: Login },
     { path: '/problem', component: ProblemList },
     { path: '/problem/:pid/edit', component: ProblemEditor },
     { path: '/problem/:pid', component: Problem },
     { path: '/submission', component: SubmissionList },
     { path: '/submission/:sid', component: Submission },
-    { path: '/user/:uid', component: User }
+    { path: '/user/:uid', component: User },
+    { path: '/:catchAll(.*)', component: Page404 }
 ];
 
 const router = createRouter({
